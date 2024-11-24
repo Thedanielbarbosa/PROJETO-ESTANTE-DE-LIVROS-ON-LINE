871e3bb29c584f0f1fcd9db9e3eef1da9859386d
Daniel Alves Barbosa <alvesbarbosa2042@gmail.com>
2024-11-23 21:35:04 -0300

    fix
    Implementado a funcionalidade de adicionar a prateleira de lendo e já lidos e o btn que adiciona e  remove

--- library/src/Home/Homepage.js
+++ library/src/Home/Homepage.js
@@ -34,6 +34,7 @@ export function Homepage(){
     
     const [books, setBooks] = useState ([]);
     const [ text, setText] = useState ('');
+     const [indiceSelecionado, setIndiceSelecionado] = useState(0);
     
     
 
@@ -43,6 +44,19 @@ export function Homepage(){
     const [livros, setLivros] = useState([]);
     const [livroSelecionado, setLivroSelecionado] = useState(null);
     const [favoritos, setFavoritos] = useState([]); // Estado para os favoritos
+    const [livroSelecionado2, setLivroSelecionado2] = useState(null);
+    const [finalizado, setFinalizado] = useState([]);
+    const [pratileira, setPratileira] = useState([]);
+
+    const moverLivro = (favoritos) => {
+      // Remove o livro de livrosDisponiveis
+      setFavoritos((prev) =>
+        prev.filter((item) => item.id !== favoritos.id)
+      );
+  
+      // Adiciona o livro à pratileira
+      setFinalizado((prev) => [...prev, favoritos]);
+    };
   
     useEffect(() => {
       const fetchLivros = async () => {
@@ -63,48 +77,52 @@ export function Homepage(){
     const fecharPopup = () => {
       setLivroSelecionado(null);
     };
+
+    const abrirPopup2 = (favoritos) => {
+      setLivroSelecionado2(favoritos);
+    };
+  
+    const fecharPopup2 = () => {
+      setLivroSelecionado2(null);
+    };
   
     // Função para adicionar livro aos favoritos
     const adicionarFavorito = (livro) => {
       if (!favoritos.some((fav) => fav.id === livro.id)) {
         setFavoritos([...favoritos, livro]);
-        alert(`"${livro.title}" foi adicionado aos favoritos!`);
+        alert(`"${livro.title}" foi adicionado a sua leitura!`);
       } else {
-        alert(`"${livro.title}" já está nos favoritos!`);
+        alert(`"${livro.title}" já está na sua leitura!`);
       }
     };
+
+    const adicionarfinalizado= (livro) => {
+      if (!finalizado.some((fan) => fan.id === livro.id)) {
+        setFinalizado([...finalizado, livro]);
+        alert(`"${livro.title}" foi adicionado aos finalizado!`);
+      } else {
+        alert(`"${livro.title}" já está nos finalizado!`);
+      }
+    };
+
+const Exibirfinalizado = ({ finalizado = [] }) => {
+      console.log(finalizado);
+      
+      if (finalizado.length === 0) {
+        return null; // Não exibe nada se não houver favoritos
+      }
   
-const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
+    };
+
+
+const ExibirFavoritos = ({ favoritos = []}) => {
         console.log(favoritos);
         
         if (favoritos.length === 0) {
           return null; // Não exibe nada se não houver favoritos
         }
-      
-        return (
-          <div className="books">
-            
-              {favoritos.map((fav) => (
-                <div key={fav.id} className="livro-item">
-                  <img
-                    src={fav.imageUrl}
-                    alt={fav.title}
-                    className="img-livro"
-                    onClick={() => abrirPopup(fav)}
-                  />
-                  
-                </div>
-              ))}
-           
-          </div>
-           
-        );
+    
       };
-
-   
-      
-
-   
     return(
     
     <div className="GridBasicStructure">
@@ -129,9 +147,19 @@ const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
                 
                 <div className ="Bookreading">
                     <div className = "mybookread">
-                        <div>
-                            <p>continue lendo</p>
-                            <p>mycont</p>
+                        <div className='continueReading'>
+                            {favoritos[0] ? (
+                              <div key={favoritos[0].id} className="livro-item">
+                                <img
+                                  src={favoritos[0].imageUrl}
+                                  alt={favoritos[0].title}
+                                  className="imgcontinue"
+                                />
+                              </div>
+                            ) : (
+                              <p>Não há livros disponíveis.</p> 
+                            )}
+                            
                         </div>
                        
                         <div className='go'>
@@ -175,7 +203,7 @@ const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
                 
 
                 <div className = "Pratileira">
-                        Reading
+                      Estou lendo:
                         <div className ="books">
                               {favoritos.map((fav) => (
                                 <div key={fav.id} className="livro-item">
@@ -183,9 +211,12 @@ const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
                                     src={fav.imageUrl}
                                     alt={fav.title}
                                     className="img-livro"
-                                    onClick={() => abrirPopup(fav)}
+                                    onClick={() => abrirPopup2(fav)}
                                   />
                                 </div>
+                                
+                                
+                                
                               ))}
                             
                             
@@ -197,7 +228,7 @@ const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
                         
                 </div>
                 <div className = "Pratileira">
-                        Books
+                Quero ler:
                         <div className ="books">
                             <div className='b'>
                               <div className="livros-lista">
@@ -218,11 +249,37 @@ const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
                         </div>
 
                         <div className ="pratileiradolivro">
-                            .
+                          
                          </div>
                         
                   </div>
-                  {livroSelecionado && (
+                  <div className = "Pratileira">
+                    Já lido:
+                        <div className ="books">
+                              {finalizado.map((fav) => (
+                                <div key={fav.id} className="livro-item">
+                                  <img
+                                    src={fav.imageUrl}
+                                    alt={fav.title}
+                                    className="img-livro"
+                                    onClick={() => abrirPopup2(fav)}
+                                  />
+                                </div>
+                                
+                                
+                                
+                              ))}
+                            
+                            
+                        </div>
+
+                        <div className ="pratileiradolivro">
+                       
+                         </div>
+                        
+                </div>
+                
+                  { ( livroSelecionado && (
                     <div className="popup" onClick={fecharPopup}>
                       <div className="popup-conteudo" onClick={(e) => e.stopPropagation()}>
                         <button className="fechar-btn" onClick={fecharPopup}>
@@ -254,16 +311,55 @@ const ExibirFavoritos = ({ favoritos = [] , abrirPopup = () => {} }) => {
                         </button>
                       </div>
                     </div>
-                  )}
+                  ))
+                }
 
+                    { ( livroSelecionado2 && (
+                    <div className="popup" onClick={fecharPopup2}>
+                      <div className="popup-conteudo" onClick={(e) => e.stopPropagation()}>
+                        <button className="fechar-btn" onClick={fecharPopup2}>
+                          x
+                        </button>
+                        <h2>{livroSelecionado2.title}</h2>
+                        <div className="inf-Livro">
+                          <div>
+                            <img
+                              src={livroSelecionado2.imageUrl}
+                              alt={livroSelecionado2.title}
+                              className="img-popup"
+                            />
+                          </div>
+                          <div className="dados-livro">
+                            <p><strong>Autor(es):</strong> {livroSelecionado2.authors?.join(", ")}</p>
+                            <p><strong>Editora:</strong> {livroSelecionado2.publisher}</p>
+                            <p><strong>Data de publicação:</strong> {livroSelecionado2.publishedDate}</p>
+                            <p><strong>Idioma:</strong> {livroSelecionado2.language}</p>
+                            <p><strong>Categorias:</strong> {livroSelecionado2.categories?.join(", ")}</p>
+                          </div>
+                        </div>
+                        <p><strong>Descrição:</strong> {livroSelecionado2.description}</p>
+                        <a href={livroSelecionado2.infoLink} target="_blank" rel="noopener noreferrer">
+                          Mais informações
+                        </a>
+                        <button className="ler-livro" onClick={() => { moverLivro(livroSelecionado2);}}>
+                          Finalizado
+                        </button>
+                      </div>
+                    </div>
+                  ))
+                }
+                
+
+                    
                 <div className = "Pratileira">
                        
                         
                 </div>
 
                 
-
+                
             </div>
+            
         </div>
 
 
